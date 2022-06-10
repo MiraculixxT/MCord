d825d369b5b90021663dafbcfc56ba4fa9aabb10
Emma Böcker <stckoverflw@gmail.com>
2022-06-10 23:33:35 +0200

    refactor gradle build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 
 plugins {
     kotlin("jvm") version "1.7.0"
+    kotlin("plugin.serialization") version "1.6.20"
     java
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.20"
 }
 
 group = "de.miraculixx"
@@ -13,19 +13,19 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("net.dv8tion:JDA:5.0.0-alpha.11")
-    implementation("org.slf4j:slf4j-nop:2.0.0-alpha7")
-    implementation("ch.qos.logback:logback-classic:1.2.11")
-    implementation("io.ktor:ktor-client-core-jvm:2.0.1")
-    implementation("io.ktor:ktor-client-cio:2.0.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
-    implementation("club.minnced:discord-webhooks:0.8.0")
+    implementation("net.dv8tion", "JDA", "5.0.0-alpha.11")
+    implementation("club.minnced", "discord-webhooks", "0.8.0")
+
+    implementation("org.jetbrains.kotlinx", "kotlinx-serialization-json", "1.3.3")
+
+    implementation("io.ktor", "ktor-client-core-jvm", "2.0.1")
+    implementation("io.ktor", "ktor-client-cio", "2.0.1")
+
+    implementation("org.slf4j", "slf4j-nop", "2.0.0-alpha7")
+    implementation("ch.qos.logback", "logback-classic", "1.2.11")
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
+
 tasks {
     jar {
         manifest {
