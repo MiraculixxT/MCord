0d118a2d798ff893317ef904f0c43baea3104c8d
Miraculixx <82050017+MiraculixxT@users.noreply.github.com>
2022-06-10 19:00:46 +0200

    Makes testing easier

---
+++ src/main/kotlin/de/miraculixx/mcord/utils/Variables.kt
@@ -0,0 +1,16 @@
+package de.miraculixx.mcord.utils
+
+import net.dv8tion.jda.api.entities.Message
+
+const val pw = "API_Token"
+
+//Just Placeholder
+val german = mapOf(
+    Pair("noPremium","")
+)
+val english = mapOf(
+    Pair("noPremium","")
+)
+
+val stupidCatching = HashMap<String, Message>()
+const val key = "Discord_Bot_Token"
