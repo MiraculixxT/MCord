6245da49865a2156a2958b2a60fb44139c8ef9f3
Miraculixx <miraculixxyt@gmail.com>
2022-07-21 15:24:31 +0200

    Improve TTT Bot AI

--- src/main/kotlin/de/miraculixx/mcord/modules/games/tictactoe/TTTBot.kt
+++ src/main/kotlin/de/miraculixx/mcord/modules/games/tictactoe/TTTBot.kt
@@ -2,14 +2,11 @@ package de.miraculixx.mcord.modules.games.tictactoe
 
 import de.miraculixx.mcord.modules.games.utils.FieldsTwoPlayer
 import de.miraculixx.mcord.modules.games.utils.getOpponent
-import de.miraculixx.mcord.utils.Color
-import de.miraculixx.mcord.utils.log
 import kotlin.random.Random
 import kotlin.random.nextInt
 
-class TTTBot(val level: Int, val player: FieldsTwoPlayer) {
+class TTTBot(val level: Int, private val player: FieldsTwoPlayer) {
     fun getMove(board: Array<Array<FieldsTwoPlayer>>): Pair<Int, Int> {
-        "Level $level - Bot Move".log()
         return when (level) {
             1 -> getRandomMove(board)
             2 -> getDangerPosition(board) ?: getRandomMove(board)
@@ -62,7 +59,6 @@ class TTTBot(val level: Int, val player: FieldsTwoPlayer) {
         // ROWS -
         repeat(3) {
             if ((countEntries(board[it], player.getOpponent()) == 2) && (countEntries(board[it], player) == 0)) {
-                "BOT - Row Danger ${board[it].lastIndexOf(FieldsTwoPlayer.EMPTY)}".log()
                 return it to board[it].lastIndexOf(FieldsTwoPlayer.EMPTY)
             }
         }
@@ -71,7 +67,6 @@ class TTTBot(val level: Int, val player: FieldsTwoPlayer) {
         val turned = (0..2).map { i -> (0..2).map { j -> board[j][i] }.toTypedArray() }
         repeat(3) {
             if ((countEntries(turned[it], player.getOpponent()) == 2) && (countEntries(turned[it], player) == 0)) {
-                "BOT - Column Danger ${turned[it].lastIndexOf(FieldsTwoPlayer.EMPTY)}".log()
                 return turned[it].lastIndexOf(FieldsTwoPlayer.EMPTY) to it
             }
         }
@@ -79,19 +74,16 @@ class TTTBot(val level: Int, val player: FieldsTwoPlayer) {
         // DIAGONALS /\
         val d1 = arrayOf(board[0][0], board[1][1], board[2][2])
         if ((countEntries(d1, player.getOpponent()) == 2) && (countEntries(d1, player) == 0)) {
-            "BOT - Diagonal 1 Danger ${d1.lastIndexOf(FieldsTwoPlayer.EMPTY) to d1.lastIndexOf(FieldsTwoPlayer.EMPTY)}".log()
             return d1.lastIndexOf(FieldsTwoPlayer.EMPTY) to d1.lastIndexOf(FieldsTwoPlayer.EMPTY)
         }
         val d2 = arrayOf(board[0][2], board[1][1], board[2][0])
         if ((countEntries(d1, player.getOpponent()) == 2) && (countEntries(d1, player) == 0)) {
-            "BOT - Diagonal 2 Danger".log()
             return when (FieldsTwoPlayer.EMPTY) {
                 d2[0] -> 0 to 2
                 d2[1] -> 1 to 1
                 else -> 2 to 0
             }
         }
-        "no danger".log(Color.RED)
         // NO DANGER DETECTED
         return null
     }
