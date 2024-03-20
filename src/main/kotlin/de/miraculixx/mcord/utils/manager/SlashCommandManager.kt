4de3b588302560110cffb22cef4b36c77d3ae456
Miraculixx <miraculixxyt@gmail.com>
2024-03-20 17:26:18 +0100

    Move MUtils commands to MUtils branche

--- src/main/kotlin/de/miraculixx/mcord/utils/manager/SlashCommandManager.kt
+++ src/main/kotlin/de/miraculixx/mcord/utils/manager/SlashCommandManager.kt
@@ -33,53 +33,6 @@ object SlashCommandManager {
         //Implement all Commands into Discord
         val jda = INSTANCE.jda
 
-        guildMCreate.updateCommands()
-            .addCommands(
-                Command("account", "Display and Manage your account settings and server connections") {
-                    subcommand("create", "Generate a new account to activate your Premium and receive a Key") {
-                        option<String>("minecraft_name", "Enter your Minecraft name. Only the given account can activate a key on servers", true)
-                    }
-                    subcommand("info", "Display and Manage your account settings and server connections")
-                    subcommand("update", "Refresh your Rank Status to activate it on your Account") {
-                        option<String>("rank", "Which rank do you want to refresh? (Spamming this command leads into blacklist)", true, true)
-                    }
-                    subcommand("email", "Change your Email - You agree to https://mutils.de/legal/privacy by entering") {
-                        option<String>("email", "Your new email - You agree to https://mutils.de/legal/privacy by entering", true)
-                    }
-                },
-                Commands.slash("admin", "A Admin only command for testing")
-                    .addOption(OptionType.STRING, "call", "Action to do", true, true)
-                    .addOption(OptionType.BOOLEAN, "status", "Switch Online Status of MUtils")
-                    .addOption(OptionType.ROLE, "role", "Some role"),
-                Command("message", "Utility command for messages") {
-                    subcommand("add-link", "Add a link button to a message") {
-                        addOption(OptionType.STRING, "msg-id", "Message ID", true)
-                        addOption(OptionType.STRING, "link", "URL", true)
-                        addOption(OptionType.STRING, "label", "Button Name", true)
-                        addOption(OptionType.STRING, "emote", "Button Emote", true)
-                    }
-                    subcommand("embed-create", "Create a new Embed") {
-                        addOption(OptionType.STRING, "code", "JSON code (Empty = Dummy)")
-                        addOption(OptionType.STRING, "msg-id", "Message ID (Empty = New Message)")
-                    }
-                    subcommand("embed-edit", "Edit a existing Embed") {
-                        addOption(OptionType.STRING, "msg-id", "Message ID", true)
-                        addOption(OptionType.STRING, "title", "Title")
-                        addOption(OptionType.STRING, "title-url", "Title URL")
-                        addOption(OptionType.STRING, "description", "Description")
-                        addOption(OptionType.STRING, "color", "Color")
-                        addOption(OptionType.STRING, "footer", "Footer Text")
-                        addOption(OptionType.STRING, "footer-icon", "Footer Icon URL")
-                        addOption(OptionType.STRING, "code", "JSON Code")
-                    }
-                    subcommand("send", "Send a Message") {
-                        addOption(OptionType.ROLE, "ping", "Ping a role")
-                    }
-                    subcommand("remove-buttons", "Remove all Buttons") {
-                        addOption(OptionType.STRING, "msg-id", "Message ID", true)
-                    }
-                }
-            ).queue()
         guildMiraculixx.updateCommands()
             .addCommands(
                 Commands.slash("admin", "A Admin only command for testing")
